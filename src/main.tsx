b371e4b2497f0dae52e00e11629f6f2af22f266b
Eve9612 <ekumehh@gmail.com>
2025-06-23 18:48:47 -0500

    Added Json and Module

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename="/jsresume">
+    <BrowserRouter basename="/jsresume/">
     <App />
     </BrowserRouter>
   </StrictMode>
