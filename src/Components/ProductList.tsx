f156382fe05315c55663bbf13e3fe85099a0afd1
Eve9612 <ekumehh@gmail.com>
2025-06-27 18:19:20 -0500

    Created Product List

--- src/Components/ProductList.tsx
+++ src/Components/ProductList.tsx
@@ -1,2 +1,27 @@
-const ProductList = () => <h1>Here are the products</h1>
-export default ProductList; 
+import { useEffect } from "react";
+
+const ProductList: React.FC = () => {
+    useEffect(() => {
+        fetch('/products.json')
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then((data) => {
+                console.log("Products:", data);
+            })
+            .catch((error) => {
+                console.error("Failed to fetch products:", error);
+            });
+    }, []); // Runs once on mount
+
+    return (
+        <div>
+            {/* Add UI logic to display products here */}
+        </div>
+    );
+};
+
+export default ProductList;
